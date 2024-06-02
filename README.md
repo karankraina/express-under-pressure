e50acf9ecc1efad95f254f32dce2a1f3a606f0c3
Karan Raina <karanraina@gofynd.com>
2024-06-02 21:32:09 +0530

    Create a middleware register function

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# ExpressJS Under Pressure Middleware
+# ExpressJS Under Pressure
 
-This TypeScript package provides an ExpressJS middleware to monitor your server's health and automatically respond with a "Server Under Pressure" message when certain thresholds are exceeded. This is useful for maintaining server stability under heavy load.
+Monitor your server's health and automatically respond with a "Server Under Pressure" message when certain thresholds are exceeded. This is useful for maintaining server stability under heavy load.
 
 ## Features
 
@@ -26,9 +26,9 @@ yarn add express-under-pressure
 
 ## Usage
 
-Here's a detailed example of how to use the `underPressure` middleware in an Express application.
+Here's a detailed example of how to use the `underPressure` in an Express application.
 
-### Importing the Middleware
+### Import
 
 First, import the necessary modules in your TypeScript file:
 
@@ -37,28 +37,19 @@ import express from 'express';
 import { underPressure } from 'express-under-pressure';
 ```
 
-### Creating the Middleware
+### Register the middleware
 
 Next, create an instance of the middleware with your desired options:
 
 ```typescript
-const underPressureMiddleware = underPressure({
+const app = express();
+
+underPressure(app, {
   maxEventLoopDelay: 1000, // Maximum event loop delay in milliseconds
   maxHeapUsedBytes: 200 * 1024 * 1024, // Maximum heap used in bytes
   maxRssBytes: 300 * 1024 * 1024, // Maximum RSS memory used in bytes
   message: 'Server Under Pressure', // Custom response message
 });
-```
-
-### Applying the Middleware
-
-Finally, apply the middleware to your Express application:
-
-```typescript
-const app = express();
-
-// Apply the underPressure middleware
-app.use(underPressureMiddleware);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -67,44 +58,17 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
-```
-
-### Full Example
-
-Here is a complete example:
-
-```typescript
-import express from 'express';
-import { underPressure } from 'express-under-pressure';
-
-const app = express();
-
-const underPressureMiddleware = underPressure({
-  maxEventLoopDelay: 1000, // Maximum event loop delay in milliseconds
-  maxHeapUsedBytes: 200 * 1024 * 1024, // Maximum heap used in bytes
-  maxRssBytes: 300 * 1024 * 1024, // Maximum RSS memory used in bytes
-  message: 'Server Under Pressure', // Custom response message
-});
-
-app.use(underPressureMiddleware);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
-```
 
 ## Middleware Options
 
-The `underPressure` middleware accepts an options object with the following properties:
+The `underPressure` function takes express app instance and an options object with the following properties:
 
-- `maxEventLoopDelay` (number): The maximum event loop delay in milliseconds. Default is `1000` ms.
-- `maxHeapUsedBytes` (number): The maximum heap memory usage in bytes. Default is `200 * 1024 * 1024` bytes (200 MB).
-- `maxRssBytes` (number): The maximum RSS memory usage in bytes. Default is `300 * 1024 * 1024` bytes (300 MB).
-- `message` (string): The message to send when the server is under pressure. Default is `'Server Under Pressure'`.
+- `maxEventLoopDelay` (number): The maximum event loop delay in milliseconds.
+- `maxHeapUsedBytes` (number): The maximum heap memory usage in bytes.
+- `maxRssBytes` (number): The maximum RSS memory usage in bytes.
+- `maxEventLoopUtilization` (number): The maximum event loop utilisation.
+- `message` (string): The message to send when the server is under pressure.
+- `retryAfter` (number): The value for Retry-After header..
 
 ## Contributing
 
@@ -119,3 +83,4 @@ This project is licensed under the MIT License. See the [LICENSE](LICENSE) file 
 Feel free to reach out if you have any questions or need further assistance.
 
 Happy coding!
+```
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,137 @@
-import type { Request, Response, NextFunction } from 'express';
+import type { Request, Response, NextFunction, Express } from 'express';
+import { performance, monitorEventLoopDelay } from 'node:perf_hooks';
+
+const { eventLoopUtilization } = performance;
+
+const SERVICE_UNAVAILABLE = 503;
+
+// const TYPE_EVENT_LOOP_DELAY = 'eventLoopDelay';
+// const TYPE_HEAP_USED_BYTES = 'heapUsedBytes';
+// const TYPE_RSS_BYTES = 'rssBytes';
+// const TYPE_HEALTH_CHECK = 'healthCheck';
+// const TYPE_EVENT_LOOP_UTILIZATION = 'eventLoopUtilization';
 
 interface MiddlewareOptions {
   maxEventLoopDelay?: number;
+  maxEventLoopUtilization?: number;
   maxHeapUsedBytes?: number;
   maxRssBytes?: number;
+  retryAfter?: number;
   message?: string;
 }
 
-type UnderPressureMiddleware = (
-  request: Request,
-  response: Response,
-  next: NextFunction,
-) => unknown;
-
-const defaultOptions = {
-  maxEventLoopDelay: 1000,
-  maxHeapUsedBytes: 200 * 1024 * 1024,
-  maxRssBytes: 300 * 1024 * 1024,
-  message: 'Server Under Pressure',
-};
-
-function checkEventLoopDelay(maxDelay: number): boolean {
-  const start = process.hrtime();
-  setTimeout(() => {
-    const delta = process.hrtime(start);
-    const delay = delta[0] * 1e3 + delta[1] / 1e6;
-    return delay > maxDelay;
-  }, 0);
-  return false;
-}
-
 export function underPressure(
+  app: Express,
   opts: MiddlewareOptions = {},
-): UnderPressureMiddleware {
-  const options = Object.assign({}, defaultOptions, opts);
+): void {
+  const resolution = 10;
+  const sampleInterval = 1000;
+  const maxEventLoopDelay = opts.maxEventLoopDelay || 0;
+  const maxHeapUsedBytes = opts.maxHeapUsedBytes || 0;
+  const maxRssBytes = opts.maxRssBytes || 0;
+  const maxEventLoopUtilization = opts.maxEventLoopUtilization || 0;
+  const message = opts.message || 'Service Unavailable';
 
-  return function underPressureHandler(request, response, next) {
-    console.log('Under Pressure working...');
-    const { heapUsed, rss } = process.memoryUsage();
+  const checkMaxEventLoopDelay = maxEventLoopDelay > 0;
+  const checkMaxHeapUsedBytes = maxHeapUsedBytes > 0;
+  const checkMaxRssBytes = maxRssBytes > 0;
+  const checkMaxEventLoopUtilization = maxEventLoopUtilization > 0;
 
-    const isEventLoopDelayed = checkEventLoopDelay(options.maxEventLoopDelay);
+  console.log({
+    checkMaxEventLoopDelay,
+    checkMaxEventLoopUtilization,
+    checkMaxHeapUsedBytes,
+    checkMaxRssBytes,
+  });
+
+  if (
+    checkMaxEventLoopUtilization === false &&
+    checkMaxEventLoopDelay === false &&
+    checkMaxHeapUsedBytes === false &&
+    checkMaxRssBytes === false
+  ) {
+    return;
+  }
+
+  let heapUsed = 0;
+  let rssBytes = 0;
+  let eventLoopDelay = 0;
+
+  let eventLoopUtilized = 0;
+
+  const histogram = monitorEventLoopDelay({ resolution });
+  histogram.enable();
+
+  const elu = eventLoopUtilization();
+
+  const timer = setTimeout(beginMemoryUsageUpdate, sampleInterval);
+  timer.unref();
+  beginMemoryUsageUpdate();
+
+  const retryAfter = opts.retryAfter || 10;
+
+  function updateEventLoopDelay() {
+    eventLoopDelay = Math.max(0, histogram.mean / 1e6 - resolution);
+    if (Number.isNaN(eventLoopDelay)) eventLoopDelay = Infinity;
+    histogram.reset();
+  }
+
+  function updateEventLoopUtilization() {
+    if (elu) {
+      eventLoopUtilized = eventLoopUtilization(elu).utilization;
+    } else {
+      eventLoopUtilized = 0;
+    }
+  }
+
+  function beginMemoryUsageUpdate() {
+    updateMemoryUsage();
+    timer.refresh();
+  }
+
+  function updateMemoryUsage() {
+    const mem = process.memoryUsage();
+    console.log('updating heap used: ', mem.heapUsed);
+    heapUsed = mem.heapUsed;
+    rssBytes = mem.rss;
+    updateEventLoopDelay();
+    updateEventLoopUtilization();
+  }
+
+  function handlePressure(request: Request, response: Response) {
+    response.setHeader('Retry-After', retryAfter);
+    console.log({ message });
+    response.status(SERVICE_UNAVAILABLE).send(message);
+  }
+
+  function underPressureHandler(
+    request: Request,
+    response: Response,
+    next: NextFunction,
+  ) {
+    console.log({ heapUsed });
+    // TODO: Pass info about what resource caused this pressure.
+    if (checkMaxEventLoopDelay && eventLoopDelay > maxEventLoopDelay) {
+      return handlePressure(request, response);
+    }
+
+    if (checkMaxHeapUsedBytes && heapUsed > maxHeapUsedBytes) {
+      return handlePressure(request, response);
+    }
+
+    if (checkMaxRssBytes && rssBytes > maxRssBytes) {
+      return handlePressure(request, response);
+    }
 
     if (
-      heapUsed > options.maxHeapUsedBytes ||
-      rss > options.maxRssBytes ||
-      isEventLoopDelayed
+      checkMaxEventLoopUtilization &&
+      eventLoopUtilized > maxEventLoopUtilization
     ) {
-      response.status(503).send(options.message);
-    } else {
-      next();
+      return handlePressure(request, response);
     }
-  };
+
+    next();
+  }
+
+  app.use(underPressureHandler);
 }
